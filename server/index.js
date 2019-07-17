916e91ad3b5adef61c6679ec8b1b71f4f7f2998e
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-07-17 15:05:15 +0200

    feat(server): Provide more information about running server
    - Add external ip for direct network usage

--- server/index.js
+++ server/index.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const os = require('os');
+
 app.use(express.static(`${__dirname}/../client/public`));
 
 app.listen(8080);
-console.log('[ INFO ] server listens on port 8080.');
+
+console.log('[INFO] Development server running!\n');
+console.log('Local:    http://localhost:8080');
+console.log(`External: http://${getIpAddress('WLAN')}:8080`);
+console.log('\nUse Ctrl+C to quit this process');
+
+function getIpAddress(name) {
+    let interfaces = os.networkInterfaces();
+    let result = interfaces[name].filter(details => {
+        return details.family == 'IPv4';
+    });
+    return result.length > 0 ? result[0].address : undefined;
+};

e292715b4b8e94a077fc2a0166faa50151ccc39b
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-07-18 12:24:48 +0200

    feat(server): Add network interface definition from CLI args

--- server/index.js
+++ server/index.js
@@ -8,15 +8,30 @@ app.use(express.static(`${__dirname}/../client/public`));
 
 app.listen(8080);
 
+let networkInterfaceName = process.argv.length > 2 ?
+    process.argv[2] : 'WLAN';
+let externalIp = getIpAddress(networkInterfaceName);
+
 console.log('[INFO] Development server running!\n');
 console.log('Local:    http://localhost:8080');
-console.log(`External: http://${getIpAddress('WLAN')}:8080`);
+
+if (externalIp) {
+    console.log(`External: http://${getIpAddress(networkInterfaceName)}:8080`);
+} else {
+    console.warn(`External: Network interface name '${networkInterfaceName}' is not valid.`);
+    console.warn(`          Use one of the following as command line argument:\n`);
+
+    let test = Object.keys(os.networkInterfaces()).forEach(value => {
+        console.log(`  - ${value}`);
+    });
+}
 console.log('\nUse Ctrl+C to quit this process');
 
 function getIpAddress(name) {
     let interfaces = os.networkInterfaces();
-    let result = interfaces[name].filter(details => {
+    let result = interfaces[name] ?
+        interfaces[name].filter(details => {
         return details.family == 'IPv4';
-    });
+    }) : [];
     return result.length > 0 ? result[0].address : undefined;
 };

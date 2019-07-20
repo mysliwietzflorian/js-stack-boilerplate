c224fd6719f06ad0c6c33ba5880dde5a4ffde461
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-07-20 04:13:40 +0200

    chore(ports): Minor fixes

--- server/index.js
+++ server/index.js
@@ -9,25 +9,25 @@ const app = express();
 app.use(express.static(`${__dirname}/../client/public`));
 
 http.createServer((req, res) => {
-    res.writeHead(301, {'Location': `https://${req.headers['host']}${req.url}`});
+    res.writeHead(301, {'Location': 'https://' + req.headers.host + req.url});
     res.end();
 }, app).listen(80);
 
 https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
-}, app).listen(443, () => initializeServer());
+}, app).listen(443, () => initializeServer(443));
 
-function initializeServer() {
+function initializeServer(port) {
     let networkInterfaceName = process.argv.length > 2 ?
         process.argv[2] : 'WLAN';
     let externalIp = getIpAddress(networkInterfaceName);
 
     console.log('[INFO] Development server running!\n');
-    console.log('Local:    https://localhost:443');
+    console.log(`Local:    https://localhost:${port}`);
 
     if (externalIp) {
-        console.log(`External: https://${getIpAddress(networkInterfaceName)}:443`);
+        console.log(`External: https://${getIpAddress(networkInterfaceName)}:${port}`);
     } else {
         console.warn(`External: Network interface name '${networkInterfaceName}' is not valid.`);
         console.warn(`          Use one of the following as command line argument:\n`);
